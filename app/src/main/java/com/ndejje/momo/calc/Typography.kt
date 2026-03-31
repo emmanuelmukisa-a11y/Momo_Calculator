11ca7f064f96a6cb2fbe25a01efe905682ea3715
emmanuelmukisa-a11y <emmanuel.mukisa@stud.ndejjeuniversity.ac.ug>
2026-03-31 11:09:29 +0300

    in typography.kt, declared font family

--- app/src/main/java/com/ndejje/momo/calc/Typography.kt
+++ app/src/main/java/com/ndejje/momo/calc/Typography.kt
@@ -1,2 +1,16 @@
 package com.ndejje.momo.calc
 
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+/*import com.ndejje.momocalc.R*/
+
+val PlayFairDisplay = FontFamily(
+    Font(R.font.playfair_display_regular, FontWeight.Normal),
+    Font(R.font.playfair_display_bold, FontWeight.Bold)
+)
+
+val SourceSans3 = FontFamily(
+    Font(R.font.source_sans3_regular, FontWeight.Normal),
+    Font(R.font.source_sans3_semibold, FontWeight.SemiBold)
+)
