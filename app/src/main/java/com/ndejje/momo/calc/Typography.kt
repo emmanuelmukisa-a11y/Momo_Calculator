4fc2df82676f1621093115589a4feff69551904e
emmanuelmukisa-a11y <emmanuel.mukisa@stud.ndejjeuniversity.ac.ug>
2026-03-31 11:32:58 +0300

    finished building customised momo typography

--- app/src/main/java/com/ndejje/momo/calc/Typography.kt
+++ app/src/main/java/com/ndejje/momo/calc/Typography.kt
@@ -1,9 +1,13 @@
 package com.ndejje.momo.calc
 
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-/*import com.ndejje.momocalc.R*/
+import androidx.compose.ui.unit.sp
+/*
+import com.ndejje.momocalc.R*/
 
 val PlayFairDisplay = FontFamily(
     Font(R.font.playfair_display_regular, FontWeight.Normal),
@@ -13,4 +17,30 @@ val PlayFairDisplay = FontFamily(
 val SourceSans3 = FontFamily(
     Font(R.font.source_sans3_regular, FontWeight.Normal),
     Font(R.font.source_sans3_semibold, FontWeight.SemiBold)
+)
+
+val MomoTypography = Typography(
+    headlineMedium = TextStyle(
+        fontFamily = PlayFairDisplay,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = SourceSans3,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = SourceSans3,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+
+    bodySmall = TextStyle(
+        fontFamily = SourceSans3,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
 )
